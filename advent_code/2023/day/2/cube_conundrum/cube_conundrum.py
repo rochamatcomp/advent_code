8bebf1cd1b2cbe6929e9bae326c3e76f8b5e08c0
André Rocha <rocha.matcomp@gmail.com>
2023-12-04 18:12:15 -0300

    Add part 2 tests and code
    Get fewest set, calculate power set, summation of power sets.

--- advent_code/2023/day/2/cube_conundrum/cube_conundrum.py
+++ advent_code/2023/day/2/cube_conundrum/cube_conundrum.py
@@ -25,9 +25,28 @@ them back in the bag. He'll do this a few times per game.
 Determine which games would have been possible if the bag had been loaded with
 only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the
 IDs of those games?
+
+--- Part Two ---
+
+The Elf says they've stopped producing snow because they aren't getting any
+water! He isn't sure why the water stopped; however, he can show you how to
+get to the water source to check it out for yourself. It's just up ahead!
+
+As you continue your walk, the Elf poses a second question: in each game you
+played, what is the fewest number of cubes of each color that could have been
+in the bag to make the game possible?
+
+The power of a set of cubes is equal to the numbers of red, green,
+and blue cubes multiplied together. The power of the minimum set of cubes in
+game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively.
+Adding up these five powers produces the sum 2286.
+
+For each game, find the minimum set of cubes that must have been present.
+What is the sum of the power of these sets?
 """
 
 from dataclasses import dataclass
+from itertools import tee
 from typing import Iterator
 
 
@@ -113,3 +132,45 @@ def check(game_raw: str, red=12, green=13, blue=14) -> int:
         return 0
 
     return game_id
+
+
+def get_fewest_game(game_raw: str) -> Game:
+    name, _ = game_raw.split(':')
+    game_id = parse_name(name)
+    subsets = parse(game_raw)
+
+    reds, greens, blues = tee(subsets, 3)
+
+    red = max(subset.red for subset in reds)
+    green = max(subset.green for subset in greens)
+    blue = max(subset.blue for subset in blues)
+
+    game = Game(
+        game_id,
+        red,
+        green,
+        blue
+    )
+
+    return game
+
+
+def calculate_power_set(game_raw: str) -> int:
+    game = get_fewest_game(game_raw)
+    power = game.red * game.green * game.blue
+
+    return power
+
+
+def calculate_power_sets(games: list[str]) -> Iterator[int]:
+    for game in games:
+        power = calculate_power_set(game)
+
+        yield power
+
+
+def total_power_sets(games: list[str]) -> int:
+    total = sum(calculate_power_sets(games))
+
+    return total
+
--- advent_code/2023/day/2/tests/test_cube_conundrum.py
+++ advent_code/2023/day/2/tests/test_cube_conundrum.py
@@ -27,9 +27,43 @@ impossible because at one point the Elf showed you 20 red cubes at once;
 similarly, game 4 would also have been impossible because the Elf showed you
 15 blue cubes at once. If you add up the IDs of the games that would have
 been possible, you get 8.
+
+--- Part Two ---
+
+As you continue your walk, the Elf poses a second question: in each game you
+played, what is the fewest number of cubes of each color that could have been
+in the bag to make the game possible?
+
+Again consider the example games from earlier:
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+
+In game 1, the game could have been played with as few as 4 red, 2 green,
+and 6 blue cubes. If any color had even one fewer cube, the game would have
+been impossible.
+Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue
+cubes.
+Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+
+The power of a set of cubes is equal to the numbers of red, green,
+and blue cubes multiplied together. The power of the minimum set of cubes in
+game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively.
+Adding up these five powers produces the sum 2286.
 """
 
-from cube_conundrum.cube_conundrum import check
+from cube_conundrum.cube_conundrum import (
+    Game,
+    check,
+    get_fewest_game,
+    calculate_power_set,
+    total_power_sets,
+)
 
 
 def test_summation_possible_games_must_equal_8():
@@ -38,7 +72,7 @@ def test_summation_possible_games_must_equal_8():
         'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
         'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
         'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
-        'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
+        'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
     ]
 
     result = sum(check(game_raw) for game_raw in games)
@@ -55,3 +89,83 @@ def test_summation_impossible_games_must_equal_0():
     result = sum(check(game_raw) for game_raw in games)
 
     assert result == 0
+
+
+def test_fewest_game_for_game1_must_be_6_blues_4_reds_2_greens():
+    game_raw = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
+
+    fewest = Game(
+        game_id=1,
+        red=4,
+        green=2,
+        blue=6,
+    )
+
+    result = get_fewest_game(game_raw)
+
+    assert result == fewest
+
+
+def test_power_set_game1_must_be_48():
+    game_raw = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
+    power = 48
+
+    result = calculate_power_set(game_raw)
+
+    assert result == power
+
+
+def test_power_set_game2_must_be_12():
+    game_raw = 'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue'
+    power = 12
+
+    result = calculate_power_set(game_raw)
+
+    assert result == power
+
+
+def test_power_set_game3_must_be_1560():
+    game_raw = (
+        'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red'
+    )
+    power = 1560
+
+    result = calculate_power_set(game_raw)
+
+    assert result == power
+
+
+def test_power_set_game4_must_be_630():
+    game_raw = (
+        'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red'
+    )
+    power = 630
+
+    result = calculate_power_set(game_raw)
+
+    assert result == power
+
+
+def test_power_set_game5_must_be_36():
+    game_raw = 'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
+    power = 36
+
+    result = calculate_power_set(game_raw)
+
+    assert result == power
+
+
+def test_total_power_set_games_must_be_2286():
+    games = [
+        'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+        'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+        'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+        'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+        'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
+    ]
+
+    total = 2286
+
+    result = total_power_sets(games)
+
+    assert result == total
